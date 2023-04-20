eefa746a4f85861bdf08fef6fb42ed0c20aab739
Simsmeister <simon.sand.andersen@hotmail.com>
2023-04-20 13:15:42 +0200

    SoundObstacle Opdatering

---
+++ APDP4/Assets/MinimalPairs.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class MinimalPairs : MonoBehaviour
+{
+    // Define the arrays of objects
+    public GameObject[] objects1;
+    public GameObject[] objects2;
+    public GameObject[] objects3;
+
+    // Define the array of arrays
+    public GameObject[][] objectCombinations;
+
+    // Define the array of transforms for the lanes
+    public Transform[] laneTransforms;
+    public string[] transformNames;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    laneTransforms = new Transform[3]; // create an array with 3 elements
+    transformNames = new string[] {"LeftLane", "MiddleLane", "RightLane" }; // set the names of the transforms
+
+    // add Transforms to the array by name
+    for (int i = 0; i < laneTransforms.Length; i++)
+    {
+        GameObject obj = GameObject.Find(transformNames[i]);
+        if (obj != null)
+        {
+            laneTransforms[i] = obj.transform;
+        }
+    }
+        // Populate the array of arrays with the object arrays
+        objectCombinations = new GameObject[][] { objects1, objects2, objects3 };
+
+        // Randomly distribute the objects on all three lanes
+        for (int i = 0; i < laneTransforms.Length; i++)
+        {
+            int randomIndex = Random.Range(0, objectCombinations.Length);
+            GameObject[] chosenArray = objectCombinations[randomIndex];
+
+            int randomObjectIndex = Random.Range(0, chosenArray.Length);
+            Vector3 positionOffset = new Vector3(0f, randomObjectIndex * 1.5f, 0f);
+            Vector3 objectPosition = laneTransforms[i].position + positionOffset;
+            Instantiate(chosenArray[randomObjectIndex], objectPosition, Quaternion.identity);
+        }
+    }
+}
--- APDP4/Assets/Scripts/ObstacleGenerator.cs
+++ APDP4/Assets/Scripts/ObstacleGenerator.cs
@@ -9,6 +9,7 @@ public class ObstacleGenerator : MonoBehaviour
     public float spacing = 5f; // Space between obstacle generations
     public GameObject[] schoolObstacles; // Array of obstacles for school terrain
     public GameObject[] trafficObstacles; // Array of obstacles for traffic terrain
+    public GameObject[] soundObstacles;
     public float soundObstacleSpacing = 20f; // Space between sound obstacle generations
 
     private float nextSpawn = 1f; // Time until next obstacle generation
@@ -26,47 +27,51 @@ public class ObstacleGenerator : MonoBehaviour
     void Update () 
     {
         if (Time.time > nextSpawn) 
-        {
-            Vector2 randomCircle = Random.insideUnitCircle;
-            int laneIndex = Mathf.RoundToInt(Mathf.Clamp01(randomCircle.x + 0.5f) * (lanes.Length - 1));
-
-            //int laneIndex = Random.Range(0, lanes.Length); // Pick a random lane to spawn the obstacle in
-            Debug.Log(laneIndex);
-
-            // Check the layer of the terrain that the player is about to approach
-            RaycastHit hit;
-            if (Physics.Raycast(player.position + new Vector3(0, 0, 20f), Vector3.forward, out hit)) 
             {
-                currentTerrainLayer = hit.transform.gameObject.layer;
-            }
+                Vector2 randomCircle = Random.insideUnitCircle;
+                int laneIndex = Mathf.RoundToInt(Mathf.Clamp01(randomCircle.x + 0.5f) * (lanes.Length - 1));
 
-            GameObject obstacleToSpawn;
+                // Check the layer of the terrain that the player is about to approach
+                RaycastHit hit;
+                if (Physics.Raycast(player.position + new Vector3(0, 0, 20f), Vector3.forward, out hit)) 
+                {
+                    currentTerrainLayer = hit.transform.gameObject.layer;
+                }
 
+                bool isSoundObstacle = Random.value < 0.1f;
+                if (isSoundObstacle) 
+                {
+                    obstacleType = 2;
+                    nextSpawn = Time.time + soundObstacleSpacing;
+                } 
+                else 
+                {
+                    obstacleType = currentTerrainLayer == LayerMask.NameToLayer("SchoolTerrain") ? 0 : 1;
+                    nextSpawn = Time.time + spacing;
+                }
 
-            if (obstacleType == 2) 
-            { // If it's a sound obstacle, pause obstacle generation for longer
-                nextSpawn = Time.time + soundObstacleSpacing;
-            } 
-            else 
-            {
-                nextSpawn = Time.time + spacing;
-            }
+                // Determine which obstacles to spawn
+                bool spawnOtherLanes = false; // flag to determine if obstacle should be spawned in other lanes
 
-            // Determine which obstacles to spawn
-            bool spawnOtherLanes = false; // flag to determine if obstacle should be spawned in other lanes
+                GameObject obstacleToSpawn;
+                if (obstacleType == 0) 
+                { // School terrain
+                    obstacleToSpawn = schoolObstacles[Random.Range(0, schoolObstacles.Length)];
+                    spawnOtherLanes = Random.value < 0.1f;
+                } 
+                else if (obstacleType == 1) 
+                { // Traffic terrain
+                    obstacleToSpawn = trafficObstacles[Random.Range(0, trafficObstacles.Length)];
+                    spawnOtherLanes = Random.value < 0.6f;
+                } 
+                else 
+                { // Sound obstacle
+                    obstacleToSpawn = soundObstacles[Random.Range(0, soundObstacles.Length)];
+                    Debug.Log("Sound Obstacle!");
+                }
 
-            if (currentTerrainLayer == LayerMask.NameToLayer("SchoolTerrain")) 
-            { // School terrain
-                obstacleToSpawn = schoolObstacles[Random.Range(0, schoolObstacles.Length)];
-                spawnOtherLanes = Random.value < 0.1f;
-            } 
-
-            else 
-            { // Traffic terrain
-                obstacleToSpawn = trafficObstacles[Random.Range(0, trafficObstacles.Length)];
-                spawnOtherLanes = Random.value < 0.6f;
-            }
-
+                // Spawn the obstacles
+                Instantiate(obstacleToSpawn, lanes[laneIndex].position + new Vector3(-1.6f, 4.181f, player.position.z + 50f), Quaternion.identity);
 
             GameObject otherObstacleToSpawn = null;
             GameObject thirdObstacleToSpawn = null;
